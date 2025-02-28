75409518c71d1f1934222b65db59a1e9fddf110b
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2025-02-28 10:24:21 +0530

    Single and Multidimensional Arrays

---
+++ src/main/java/com/java/proceduralprogramming/assignments/Assignment3.java
@@ -0,0 +1,11 @@
+package com.java.proceduralprogramming.assignments;
+
+public class Assignment3 {
+    // EMPLOYEE ARRAY
+    // id, name, email, phone, salary, position
+    // 1, sid, sid@test.com, 830218213, 2000, "Engineer"
+    // 2...
+    // Print - Average Salary of Employees
+    // Print - All Email Id's
+    // Print - Position from Highest to Lowest
+}
