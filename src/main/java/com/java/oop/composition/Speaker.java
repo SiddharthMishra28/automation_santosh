19d9b9037d2c72e0c0b10d500766ae76c3ef36c7
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2025-04-07 09:39:10 +0530

    Java Collections Session 1

---
+++ src/main/java/com/java/oop/composition/Speaker.java
@@ -0,0 +1,22 @@
+package com.java.oop.composition;
+
+public class Speaker {
+    private String type;
+    private String brand;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
