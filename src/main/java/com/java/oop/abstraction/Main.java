948285493d619b99ab3f75845e934698feb0a735
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2025-04-23 10:23:07 +0530

    SELENIUM Initialization

--- src/main/java/com/java/oop/abstraction/Main.java
+++
@@ -1,9 +0,0 @@
-package com.java.oop.abstraction;
-
-public class Main {
-    public static void main(String[] args) {
-        Http browser = new Chrome(); // CONVENIENT FOR CHANGES
-        browser.getMethod();
-        browser.deleteMethod();
-    }
-}
--- src/main/java/com/java/oop/encapsulation/Employee.java
+++
@@ -1,4 +0,0 @@
-package com.java.oop.encapsulation;
-
-public class Employee extends Person {
-}
